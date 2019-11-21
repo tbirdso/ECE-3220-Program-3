670fb76902314b5b9cc9fb952d7494e4821e3f8f
Tom Birdsong <tbirdso@titan3.cs.clemson.edu>
2019-11-21 14:53:54 -0500

    Added provided header files and skeleton code

---
+++ tfs_2.c
@@ -0,0 +1,126 @@
+#include "tfs.h"
+
+
+/* implementation of assigned functions */
+
+
+/* you are welcome to use helper functions of your own */
+
+
+
+/* tfs_delete()
+ *
+ * deletes a closed directory entry having the given file descriptor
+ *   (changes the status of the entry to unused) and releases all
+ *   allocated file blocks
+ *
+ * preconditions:
+ *   (1) the file descriptor is in range
+ *   (2) the directory entry is closed
+ *
+ * postconditions:
+ *   (1) the status of the directory entry is set to unused
+ *   (2) all file blocks have been set to free
+ *
+ * input parameter is a file descriptor
+ *
+ * return value is TRUE when successful or FALSE when failure
+ */
+
+unsigned int tfs_delete( unsigned int file_descriptor ){
+
+  /* your code here */
+
+}
+
+
+/* tfs_read()
+ *
+ * reads a specified number of bytes from a file starting
+ *   at the byte offset in the directory into the specified
+ *   buffer; the byte offset in the directory entry is
+ *   incremented by the number of bytes transferred
+ *
+ * depending on the starting byte offset and the specified
+ *   number of bytes to transfer, the transfer may cross two
+ *   or more file blocks
+ *
+ * the function will read fewer bytes than specified if the
+ *   end of the file is encountered before the specified number
+ *   of bytes have been transferred
+ *
+ * preconditions:
+ *   (1) the file descriptor is in range
+ *   (2) the directory entry is open
+ *   (3) the file has allocated file blocks
+ *
+ * postconditions:
+ *   (1) the buffer contains bytes transferred from file blocks
+ *   (2) the specified number of bytes has been transferred
+ *         except in the case that end of file was encountered
+ *         before the transfer was complete
+ *
+ * input parameters are a file descriptor, the address of a
+ *   buffer of bytes to transfer, and the count of bytes to
+ *   transfer
+ *
+ * return value is the number of bytes transferred
+ */
+
+unsigned int tfs_read( unsigned int file_descriptor,
+                       char *buffer,
+                       unsigned int byte_count ){
+
+  /* your code here */
+
+}
+
+/* tfs_write()
+ *
+ * writes a specified number of bytes from a specified buffer
+ *   into a file starting at the byte offset in the directory;
+ *   the byte offset in the directory entry is incremented by
+ *   the number of bytes transferred
+ *
+ * depending on the starting byte offset and the specified
+ *   number of bytes to transfer, the transfer may cross two
+ *   or more file blocks
+ *
+ * furthermore, depending on the starting byte offset and the
+ *   specified number of bytes to transfer, additional file
+ *   blocks, if available, will be added to the file as needed;
+ *   in this case, the size of the file will be adjusted
+ *   based on the number of bytes transferred beyond the
+ *   original size of the file
+ *
+ * the function will read fewer bytes than specified if file
+ *   blocks are not available
+ *
+ * preconditions:
+ *   (1) the file descriptor is in range
+ *   (2) the directory entry is open
+ *   (3) the file has allocated file blocks
+ *
+ * postconditions:
+ *   (1) the file contains bytes transferred from the buffer
+ *   (2) the specified number of bytes has been transferred
+ *         except in the case that file blocks needed to
+ *         complete the transfer were not available
+ *   (3) the size of the file is increased as appropriate
+ *         when transferred bytes extend beyond the previous
+ *         end of the file
+ *
+ * input parameters are a file descriptor, the address of a
+ *   buffer of bytes to transfer, and the count of bytes to
+ *   transfer
+ *
+ * return value is the number of bytes transferred
+ */
+
+unsigned int tfs_write( unsigned int file_descriptor,
+                        char *buffer,
+                        unsigned int byte_count ){
+
+  /* your code here */
+
+}
